a7511fa3af65b24e7b25ada7cc9ebdaba224860e
connorgutman <connorgutman@gmail.com>
2016-08-15 08:43:04 -0700

    two line alert

--- js/jquery.scalem.js
+++ js/jquery.scalem.js
@@ -6,13 +6,26 @@
  * Read a copy of the license in the LICENSE file or at
  * http://choosealicense.com/licenses/mit
  */
+var lineLimit = 25;
+var messageContent = $("#alert-text").html();
+var charCount = $("#alert-text").html().length;
+if (charCount > lineLimit) {
+  lineLimit = Math.ceil((charCount / 2)+5);
+}
+if (charCount > lineLimit) {
+  var testEval = 'messageContent = messageContent.replace(/(.{1,' + lineLimit + '})(?:' + "&#92;" + 'n|$| )/g, "$1<br>");';
+  messageContent = eval(testEval);
+  $("#alert-text").html(messageContent);
+  $("#alert-message").css("height", "40vh");
+}
+
 
 (function($) {
   $.fn.scalem = function(oOptions) {
     var oSettings = $.extend({
-        ratio: 1,        // Scale ratio (1 = 100%)
+        ratio: 1, // Scale ratio (1 = 100%)
         reference: null, // Text will scale relative to this element
-        styles: ''       // List of styles to scale (useful for buttons)
+        styles: '' // List of styles to scale (useful for buttons)
       }, oOptions),
       updateStyles = function(o, e) {
         var $o = $(o),
@@ -37,23 +50,23 @@
         // Validate ratio
         if (isNaN(nRatio)) nRatio = 1;
         // Account for scrollbar?
-        if ($oP[0].scrollHeight>$oP.height()) nRefWidth -= 17;
+        if ($oP[0].scrollHeight > $oP.height()) nRefWidth -= 17;
         nTargetWidth = nRefWidth * nRatio;
         // Append clone to body to get inline width
         $o2.appendTo('body');
         nTextWidth = $o2.width();
         // Exit if something doesn't look right
-        if (nTargetWidth===0 || nTextWidth===nRefWidth) {
+        if (nTargetWidth === 0 || nTextWidth === nRefWidth) {
           $o2.remove();
           return;
         }
         // Scale the text! (6px is minimum font size to get accurate ratio)
-        for (var i=Math.round((6/$o2.css('font-size', '6px').width())*nTargetWidth), o2=$o2[0]; i<nTargetWidth; ++i) {
+        for (var i = Math.round((6 / $o2.css('font-size', '6px').width()) * nTargetWidth), o2 = $o2[0]; i < nTargetWidth; ++i) {
           // Update font-size using native method for better performance
           // (see http://jsperf.com/style-vs-csstext-vs-setattribute)
           o2.style.fontSize = i + 'px';
           if ($o2.width() / nRefWidth > nRatio) {
-            $o.css('font-size', (i-1) + 'px');
+            $o.css('font-size', (i - 1) + 'px');
             break;
           }
         }
@@ -63,9 +76,9 @@
         if (aStyles[0]) {
           var nScale = $o.width() / nTextWidth,
             oStyles = {};
-          for (var i=0, imax=aStyles.length; i<imax; ++i) {
+          for (var i = 0, imax = aStyles.length; i < imax; ++i) {
             if (!aStyles[i]) continue;
-            oStyles[aStyles[i]] = ((aStyles[i]==='width') ? nTargetWidth : Math.round(parseFloat($o.css(aStyles[i])) * nScale)) + 'px';
+            oStyles[aStyles[i]] = ((aStyles[i] === 'width') ? nTargetWidth : Math.round(parseFloat($o.css(aStyles[i])) * nScale)) + 'px';
           }
           $o.css(oStyles);
         }
